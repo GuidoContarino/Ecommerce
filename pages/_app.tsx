4de1354af63ab5dbd8a2d9a623625834ee803799
GuidoContarino <guidoconta11@gmail.com>
2023-05-30 23:24:39 -0300

    mejorando Ui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { ChakraProvider, Container, VStack, Image, Heading, Text, Box, Divider } from '@chakra-ui/react'
 import { AppProps } from 'next/app'
 import theme from '../theme'
+
 const App: React.FC<AppProps> = ({Component , pageProps}) =>{
   return (
     <ChakraProvider theme={theme}>
-      <Box padding={4}>
-      <Container backgroundColor="white"
+      <Box padding={4} backgroundColor="black">
+      <Container backgroundColor="black"
       borderRadius="sm"
       boxShadow="md"
       marginY={4}
@@ -14,9 +15,12 @@ const App: React.FC<AppProps> = ({Component , pageProps}) =>{
       padding={4}
       >
         <VStack marginBottom={6}>
-          <Image borderRadius={9999} src='//placehold.it/128x128'/>
-          <Heading>Quimica Gr</Heading>
-          <Text>Venta de Productos de Limpieza y Belleza Corporal Online</Text>
+          <Image 
+          height={150}
+          borderRadius={9999}
+          src='../assets/image.png'/>
+          <Heading color="white">Quimica Gr</Heading>
+          <Text color="white">Venta de Productos de Limpieza y Belleza Corporal Online</Text>
         </VStack>
         <Divider marginY={6}/>
         <Component {...pageProps}/>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import { GetStaticProps } from "next";
 import Link from "next/link";
 import { Product } from "../product/types";
 import api from "../product/api";
-import { Grid, Stack, Text, Button } from "@chakra-ui/react";
+import { Grid, Stack, Text, Button, Flex, Image } from "@chakra-ui/react";
 
 interface Props {
   products: Product[];
@@ -25,22 +25,54 @@ const text = React.useMemo(() => cart
 
   
 return (
-  <Stack>
+  <Stack spacing={6}>
     <Grid templateColumns='repeat(auto-fill, minmax(240px,1fr))' gap={6}>
     {products.map (product => (
-    <Stack key={product.id} backgroundColor="gray.100" borderRadius="md" padding={4}>
-      <Text textAlign="center">{product.title}</Text>
-      <Text textAlign="center" color="green.500">{parseCurrency(product.price)}</Text>
-      <Button colorScheme="primary" onClick={() => setCart(cart => cart.concat (product))}>
+    <Stack 
+    key={product.id} 
+    border="1px solid white"
+    spacing={3} 
+    backgroundColor="black" 
+    borderRadius="md" 
+    padding={4}>
+      <Image 
+        maxHeight="unset"
+        objectFit="contain"
+        borderRadius="md" 
+        src={product.image}/>
+        alt={product.title}
+      <Stack spacing={1}>
+       <Text textAlign="center" color="white">{product.title}</Text>
+       <Text color="white" textAlign="center" fontSize={13}>{product.description}</Text>
+       <Text textAlign="center" color="blue.600">{parseCurrency(product.price)}</Text>
+      </Stack>
+      <Button 
+      colorScheme="blue" 
+      size="sm"
+      onClick={() => setCart(cart => cart.concat (product))}
+      >
         Agregar
       </Button>
     </Stack>
     ))}
   </Grid>
    {Boolean(cart.length) && (
-   <Link isExternal href={`https://wa.me/5491131066937?text=${encodeURIComponent(text)}`}>
-    <Button colorScheme="whatsapp">Completar Pedido ({cart.length} productos)</Button>
-   </Link>)}
+    <Flex 
+    position="sticky" 
+    bottom={1}
+    alignItems="center"
+    justifyContent="center" 
+    >
+      <Button 
+      as={Link}
+      isExternal
+      width="fit-content"
+      colorScheme="whatsapp"
+      href={`https://wa.me/5491130429802?text=QuimicaGr:${encodeURIComponent(text)}`}>
+      Completar Pedido ({cart.length} productos)
+      </Button>
+    </Flex>
+    )}
   </Stack>
 );
 };
@@ -49,6 +81,7 @@ export const getStaticProps: GetStaticProps = async () =>
   {
     const products = await api.list();
     return {
+      revalidate:10,
       props: {
         products,
       },
